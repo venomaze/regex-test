16aabb21a27ba8a2a6d4ecc33149c342d5548b05
venomaze <venomaze@protonmail.com>
2020-08-14 10:40:43 -0700

    Add email validation regex to sandbox

--- sandbox/index.js
+++ sandbox/index.js
@@ -1,13 +1,19 @@
 const RegexTest = require('../src');
 
 const regex = new RegexTest();
+const pattern = /^([a-zA-Z0-9])(([\-.]|[_]+)?([a-zA-Z0-9]+))*(@){1}[a-z0-9]+[.]{1}(([a-z]{2,3})|([a-z]{2,3}[.]{1}[a-z]{2,3}))$/;
 
 regex
-  .test(/o$/, 'hello')
+  .test(pattern, 'test@mail.com')
   .then(res => console.log(res))
   .catch(err => console.log(err.message));
 
 regex
-  .test(/(a+)*$/, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!')
+  .test(pattern, 'mail.com')
+  .then(res => console.log(res))
+  .catch(err => console.log(err.message));
+
+regex
+  .test(pattern, 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!')
   .then(res => console.log(res))
   .catch(err => console.log(err.message));
